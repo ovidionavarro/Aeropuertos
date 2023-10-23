00266706a3d2a3eedfbd9afde8133aee36e5c189
Ovidio <ovidionavarro90@gmail.com>
2023-10-23 16:56:35 -0200

    iniciando login

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('hola mundohghgh')
+require('dotenv').config();
+const hbs=require('hbs');
+const path=require('path');
+const express = require('express')
+
+
+
+const app = express()
+app.use(express.json())
+//midlewares
+app.set('view engine','hbs')
+hbs.registerPartials(__dirname+'/views/partials')
+    //definiendo public como carpeta publica 
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res)=>{
+    res.send('desde el app')
+})
+//pag principal
+app.get('/users',(req,res)=>{
+    res.render('users')
+})
+//llamando las rutas
+app.use(require('./routers/users'))
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server on port ${process.env.PORT}`);
+})
