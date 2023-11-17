2cf997d5e00a0446321f23b8e9cee0b081c36791
Ovidio <ovidionavarro90@gmail.com>
2023-11-17 03:39:55 -0200

    agregando ruta de los tipos

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ import cors from 'cors'
 import 'dotenv/config'
 import dbConnect from './db/init.js'
 import { AdminAuthRouter, ClientRouter, AuthRouter, TypeRouter } from './routes/index.js'
-import { Client, ClientType, User } from './models/definitions/index.js'
+import {
+  Classification,
+  Client,
+  ClientType,
+  InstallationType,
+  Passengertype,
+  ReparationType,
+  User
+} from './models/definitions/index.js'
 import ModelConstructor from './models/model-constructor.js'
 const PORT = process.env.PORT ?? 1234
 
@@ -13,7 +21,10 @@ dbConnect({ alter: false })
 const app = express()
 app.use(express.json())
 app.use(cors())
-
+app.use('/reparationType', TypeRouter(new ModelConstructor(ReparationType)))
+app.use('/passengerType', TypeRouter(new ModelConstructor(Passengertype)))
+app.use('/installationType', TypeRouter(new ModelConstructor(InstallationType)))
+app.use('/classificationShip', TypeRouter(new ModelConstructor(Classification)))
 app.use('/typeClient', TypeRouter(new ModelConstructor(ClientType)))
 app.use('/clients', ClientRouter(new ModelConstructor(Client)))
 app.use('/login', AuthRouter(new ModelConstructor(Client)))
