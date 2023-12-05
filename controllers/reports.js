ccd40f3a0d26babff3aa6ab96ed7ede6d710f0d8
Arm0101 <=>
2023-12-05 03:25:54 -0500

    Convertir a pdf y convertir a excel

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,5 +1,6 @@
 import { actions } from '../config/defaultValues.js'
-
+import PDFDocument from 'pdfkit'
+import ExcelJS from 'exceljs'
 export default class ReportsController {
   constructor(ReparationModel, WorkShopReparationModel) {
     this.ReparationModel = ReparationModel
@@ -7,31 +8,49 @@ export default class ReportsController {
   }
 
   get = async (req, res) => {
-    const { body } = req
-    // validate body
-    const { action, format } = body
+    const { query } = req
+    const { action, format } = query
+    if (format !== 'pdf' && format !== 'xlsx') {
+      return res.status(400).json({ msg: 'error' })
+    }
+    if (!actions.includes(action)) {
+      return res.status(400).json({ msg: 'error' })
+    }
+    let response
+    let title
     switch (action) {
       case actions[0]: {
-        const response = await this.getNamesLocationsOfRepairServiceAirports()
-        return res.json(response)
+        response = await this.getNamesLocationsOfRepairServiceAirports()
+        title = 'Aeropuertos que brindan servicios de reparacion'
+        break
       }
       case actions[1]: {
-        const response = await this.getTotalCapitalRepairsPerAirport('capital')
-        return res.json(response)
+        response = await this.getTotalCapitalRepairsPerAirport('capital')
+        title = 'Cantidad de reparaciones capitales por aeropuerto'
+        break
       }
       case actions[2]: {
-        const response = await this.getJoseMartiAirportClientsByTypeAndShip()
-        return res.json(response)
+        response = await this.getJoseMartiAirportClientsByTypeAndShip()
+        title = 'Consulta3'
+        break
       }
       case actions[3]: {
-        const response = await this.getAirportsWithLeastTrafficAndServices()
-        return res.json(response)
+        response = await this.getAirportsWithLeastTrafficAndServices()
+        title = 'Consulta4'
+        break
       }
 
       case actions[4]: {
-        const response = await this.getAverageCostOfInefficientServicesAtJoseMarti()
-        return response.json()
+        response = await this.getAverageCostOfInefficientServicesAtJoseMarti()
+        title = 'Consulta5'
+        break
       }
+    }
+    if (format === 'pdf') {
+      return this.generatePDF(res, title, response)
+    }
+    if (format === 'xlsx') {
+      return this.generateExcel(res, title, response)
     }
   }
 
@@ -55,5 +74,56 @@ export default class ReportsController {
 
   getAverageCostOfInefficientServicesAtJoseMarti = async () => {
     return { action: 'action5' }
+  }
+
+  generatePDF = async (res, title, data) => {
+    // Crear un nuevo documento PDF
+    const doc = new PDFDocument()
+
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'attachment; filename="report.pdf"')
+    doc.pipe(res)
+    doc.fontSize(16).text(title, 100, 100)
+    if (data.lenght !== 0) {
+      const keys = Object.keys(data[0])
+      const k = keys.join(' | ')
+      doc.text(k)
+      doc.moveDown()
+    }
+    data.forEach((element) => {
+      const vals = Object.values(element)
+      const _vals = vals.join(' ')
+      doc.text(_vals)
+      doc.moveDown()
+    })
+    doc.addPage()
+    doc.end()
+    return res.status(200)
+  }
+
+  generateExcel = async (res, title, data) => {
+    try {
+      const workbook = new ExcelJS.Workbook()
+      const worksheet = workbook.addWorksheet(title)
+      if (data.lenght !== 0) {
+        const keys = Object.keys(data[0])
+        worksheet.addRow(keys)
+      }
+      data.forEach((d) => {
+        const values = Object.values(d)
+        worksheet.addRow(values)
+      })
+      res.setHeader(
+        'Content-Type',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      )
+      res.setHeader('Content-Disposition', 'attachment; filename=report.xlsx')
+      await workbook.xlsx.write(res)
+      res.end()
+      return res.status(200)
+    } catch (error) {
+      console.error('Error al generar el archivo Excel:', error)
+      res.status(500).send('Error interno del servidor')
+    }
   }
 }
